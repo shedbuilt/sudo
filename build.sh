079e67387017e45fe5bf45ba10512b05af1e6938
Auston Stewart <auston@shedbuilt.net>
2017-12-22 13:53:28 -1000

    Allow members of wheel to use sudo by default

--- build.sh
+++ build.sh
@@ -11,3 +11,4 @@ make "DESTDIR=$SHED_FAKEROOT" install
 ln -sfv libsudo_util.so.0.0.0 "${SHED_FAKEROOT}/usr/lib/sudo/libsudo_util.so.0"
 rm "${SHED_FAKEROOT}/etc/sudoers"
 mv "${SHED_FAKEROOT}/etc/sudoers.dist" "${SHED_FAKEROOT}/etc/sudoers.default"
+sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" "${SHED_FAKEROOT}/etc/sudoers.default"

bd58e630d927e9af33cb6880d6becbfa901df899
Auston Stewart <auston@shedbuilt.net>
2017-12-22 06:51:14 -1000

    Updating postinstall to add wheel group

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Install sudoers file, if absent
 if [ ! -e /etc/sudoers ]; then
     install -v -m400 /etc/sudoers.default /etc/sudoers
+fi
+# Add wheel group for sudoers, if absent
+getent group wheel
+if [ $? -ne 0 ]; then
+    groupadd -g 1001 wheel
 fi

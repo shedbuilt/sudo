2bd36b4ed0459adbbef2e8ec97cbdf44b46a5ec5
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:28:31 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@
             --with-env-editor          \
             --docdir=/usr/share/doc/sudo-1.8.20p2 \
             --with-passprompt="[sudo] password for %p: "
-make -j $SHED_NUMJOBS
-make "DESTDIR=$SHED_FAKEROOT" install
-ln -sfv libsudo_util.so.0.0.0 "${SHED_FAKEROOT}/usr/lib/sudo/libsudo_util.so.0"
-rm "${SHED_FAKEROOT}/etc/sudoers"
-mv "${SHED_FAKEROOT}/etc/sudoers.dist" "${SHED_FAKEROOT}/etc/sudoers.default"
-sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" "${SHED_FAKEROOT}/etc/sudoers.default"
+make -j $SHED_NUM_JOBS
+make "DESTDIR=$SHED_FAKE_ROOT" install
+ln -sfv libsudo_util.so.0.0.0 "${SHED_FAKE_ROOT}/usr/lib/sudo/libsudo_util.so.0"
+rm "${SHED_FAKE_ROOT}/etc/sudoers"
+mv "${SHED_FAKE_ROOT}/etc/sudoers.dist" "${SHED_FAKE_ROOT}/etc/sudoers.default"
+sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" "${SHED_FAKE_ROOT}/etc/sudoers.default"
